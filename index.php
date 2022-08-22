db69e6b06bd6332ac51876f266a41151bec50527
Motya Zakharova <motyazakharova@gmail.com>
2022-08-22 03:37:18 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include("variables.php"); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello, world!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include("navbar.php"); ?>
+    <br/>
+  <div class="container">
+    <div class="jumbotron">
+  <h1 class="display-4">Welcome to Guitar Magazine!</h1>
+  <p class="lead">ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4sryn4e7vtxdyWUVc7xDwciidmrdofP7fGc4+kiTKCocpTkEo01vL7rdDyyIqXCzFTiSNgpt5cqtGJm79+Yx3R1s9L+xQMC+mgSIzA60kSGX5jqX9FCv7gFmM17ioNTPYNHmd9unkwBQ0YZ2fWt4d5kKSxmaeHPhp87c7/u9Ji/qa44LDlglR4FZXAMBN1Ygpq2SIsdUBphx3/TjSJdHd1VoAOLLNB0kNqE1yR2qJKw7jrNDDRPt17F2UDH09GhN+22NUK38RjAC67L2Yp6tSBjJn5I9Gzp/U5efVLiy6+/yPXvKYkdK40Sltrckus76xrrlPcl6uIuU5Z1Hon/pjCYeQXY0I/u6kp2vhzhS1B4pvU6V+O+k+g8WnYbkuBDpsKIx9BD7vPvd0QFxXolNI2Bv52rtFUqFAvFWcGqKKkedacOEVAL/FAdTPu2Cmf+kNGCz37hdlmvx1xYBCoQIQXSEbdqtf/6jJwzWToj+5c1EYjyr2V2yE2wQ8jZGPczE= motya@LAPTOP-I5RMG5PC
+</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+  </div>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <center>
+    <?php include("footer.php"); ?>
+  </center>
+  </body>
+</html>
+
+ 
---
+++ variables.php
@@ -0,0 +1,6 @@
+<?php
+
+$company_name = "Guitar Magazine, Inc.";
+$tagline = "Teaching Guitar since 2001";
+
+?>
